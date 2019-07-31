58f90056da88529d155344eda7011e4386024696
gaoyaqiu <30494613@qq.com>
2019-07-31 11:45:29 +0800

    新增hdfs基本操作

--- hdfs-examples/src/test/java/com/gyq/hadoop/HDFSTest.java
+++ hdfs-examples/src/test/java/com/gyq/hadoop/HDFSTest.java
@@ -1,13 +1,17 @@
 package com.gyq.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
+
+import static org.junit.Assert.assertTrue;
 
 public class HDFSTest {
 
@@ -23,8 +27,81 @@ public class HDFSTest {
         fs = FileSystem.get(new URI(HDFS_ADDRESS), configuration, "root");
     }
 
+    /**
+     * 创建目录
+     *
+     * @throws IllegalArgumentException
+     * @throws IOException
+     */
     @Test
-    public void testMkdir() throws IllegalArgumentException, IOException {
-        fs.mkdirs(new Path("/a/b"));
+    public void testMkdir() throws IOException {
+        boolean result = fs.mkdirs(new Path("/a/b"));
+        assertTrue(result);
+        close();
+    }
+
+    /**
+     * 上传文件
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testCreate() throws IOException {
+        FileInputStream in = new FileInputStream("C:\\data\\tmp\\test.txt");
+        FSDataOutputStream out = fs.create(new Path("/a/b/test.txt"));
+        IOUtils.copyBytes(in, out, 1024);
+        close();
+    }
+
+    /**
+     * 下载文件
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testDownload() throws IOException {
+        FSDataInputStream in = fs.open(new Path("/a/b/test.txt"));
+        FileOutputStream out = new FileOutputStream("c:\\data\\tmp\\test2.txt");
+        IOUtils.copyBytes(in, out, 1024);
+        close();
+    }
+
+    /**
+     * 列出所有目录及文件
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testListFiles() throws IOException {
+        listFiles(new Path("/"));
+    }
+
+    private void listFiles(Path path) throws IOException {
+        FileStatus[] fileStatuses = fs.listStatus(path);
+        for (int i = 0; i < fileStatuses.length; i++) {
+            FileStatus fileStatus = fileStatuses[i];
+            if (fileStatus.isDirectory()) {
+                System.out.println("目录: " + fileStatus.getPath());
+                listFiles(fileStatus.getPath());
+            } else {
+                System.out.println(fileStatus.getPath());
+            }
+        }
+    }
+
+    /**
+     * 删除文件
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testDelete() throws IOException {
+        boolean result = fs.delete(new Path("/a/b/test.txt"), false);
+        assertTrue(result);
+        fs.close();
+    }
+
+    private void close() throws IOException {
+        fs.close();
     }
 }
